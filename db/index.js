787849e898ea7c5186800bb2e4ea23b022b5fc93
Toni Hackett <hckt76@gmail.com>
2019-07-28 23:36:40 +0200

    conect-mongo

--- db/index.js
+++ db/index.js
@@ -1,14 +1,27 @@
-const users = require("../resources/users");
+// const users = require("../resources/users"); // conecta con el json (./resources/users)
+const mongo = require("./connect");
+const { DB_NAME } = require("./config");
 
 module.exports = {
     getUsers: function() {
+        // return users; // json
+        const db = mongo.instance().db(DB_NAME); // conceccion de mongo
+        const users = db.collection("users").find({}).toArray();
         return users;
     },
     getUserById: function(id) {
         return users.filter(user=>user._id===id);
     },
     getUserByAgeRange: function(lower = 0, higher = 99) {
-        return users.filter(user=>user.age >= lower && user.age <= higher);
+        // return users.filter(user=>user.age >= lower && user.age <= higher);
+        const db = mongo.instance().db(DB_NAME); // conceccion de mongo
+        const users = db.collection("users").find({
+            age: {
+                $gte: Number(lower),
+                $lte: Number(higher)
+            }
+        }).toArray();
+        return users;
     }
 }
 
